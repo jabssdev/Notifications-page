25e6191f5133b72486430e2d166c369191291b81
JabssDev <jordanburgoss23@gmail.com>
2024-08-19 10:49:07 -0500

    style: :art: Finish header styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
 			fontFamily: {
 				sans: ["Plus Jakarta Sans", "sans-serif"],
 			},
+			colors: {
+				primary: {
+					red: "hsl(1, 90%, 64%)",
+					blue: "hsl(219, 85%, 26%)",
+				},
+				secundary: {
+					white: "hsl(0, 0%, 100%)",
+					lgblue1: "hsl(210, 60%, 98%)",
+					lgblue2: "hsl(211, 68%, 94%)",
+					lgblue3: "hsl(205, 33%, 90%)",
+					gblue: "hsl(219, 14%, 63%)",
+					dgblue: "hsl(219, 12%, 42%)",
+					vdblue: "hsl(224, 21%, 14%)",
+				},
+			},
 		},
 	},
 	plugins: [],
